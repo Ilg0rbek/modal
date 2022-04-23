c45a52fa10d83358b2b2014ec5ffd1654069899d
Ilgorbek Abdumalikov <ilgorbekabdumalikov331@gmail.com>
2022-04-23 14:25:51 +0500

    modal

---
+++ src/modal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Modal } from 'antd';
+
+const Modals = ({ isModalVisible, setIsModalVisible }) => {
+
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    return (
+        <div>
+
+            <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+        </div>
+    )
+}
+
+export default Modals
